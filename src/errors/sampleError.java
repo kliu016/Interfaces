82e1562a5d69f0ab170194cbabedbaa31cad2c64
Liu <Kevin.Liu@molinahealthcare.com>
2018-09-07 12:50:12 -0700

    First commit

---
+++ src/errors/sampleError.java
@@ -0,0 +1,25 @@
+package errors;
+import java.util.*;
+
+public class sampleError {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int x =1;
+		
+		do {
+		
+		try {
+			System.out.println("Enter first num: ");
+			int n1 = input.nextInt();
+			System.out.println("Enter first num: ");
+			int n2 = input.nextInt();
+			int sum = n1/n2;
+			System.out.println(sum);
+			x=2;
+		}
+		catch(Exception e){
+			System.out.print("You can't do that");
+		}
+		}while(x==1);
+	}
+}
